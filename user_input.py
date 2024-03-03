39e90ae0b512d008c60b82a6c7a97757f3250aeb
Muriuki254 <98731458+Muriuki254@users.noreply.github.com>
2024-03-03 19:12:51 +0300

    Create user_input.py
    Python task one

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name ? :")
+age = input("What is your age ? :")
+location = input("What is your location ? :")
+print(f"Hello {name}, you are {age} years old. You live in {location}. You are a good person.")
